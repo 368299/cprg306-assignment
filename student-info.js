4fbbbfcef78429d24f8a83f020dcff5ea9f98684
Rubalpreet Kaur <164897467+368299@users.noreply.github.com>
2024-10-01 21:12:40 -0600

    Add files via upload

---
+++ student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Your Name: Rubalpreet Kaur</p>
+      <p>
+        GitHub Repository: <Link href="https://github.com/368299/cprg306-assignment">https://github.com/368299/cprg306-assignment</Link>
+      </p>
+    </div>
+  );
+}
